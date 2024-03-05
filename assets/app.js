a5f954e57645bb3d38d25eae02eb36c03c074359
Xelaj <ajuillard69@gmail.com>
2024-03-05 15:30:13 +0100

    feat(articles): add slider for images in article frontend

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,5 @@
 import './bootstrap.js';
+
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -11,5 +12,5 @@ const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
-
+import './javascript/swiperArticle';
 
